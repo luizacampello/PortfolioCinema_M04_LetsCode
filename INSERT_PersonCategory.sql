697e0b3e131b015318641c936811846bf46bef33
Luiza Campello <lumcampello@gmail.com>
2022-08-05 18:04:26 -0300

    Upload CREATE, ALTERS, TRIGGERS and INSERT files

---
+++ INSERT_PersonCategory.sql
@@ -0,0 +1,20 @@
+-- PersonCategory
+
+INSERT INTO personCategory (cpf, id_category) VALUES ('11111111111', 13);
+INSERT INTO personCategory (cpf, id_category) VALUES ('11111111111', 2);
+INSERT INTO personCategory (cpf, id_category) VALUES ('11111111111', 15);
+INSERT INTO personCategory (cpf, id_category) VALUES ('22222222222', 13);
+INSERT INTO personCategory (cpf, id_category) VALUES ('22222222222', 22);
+INSERT INTO personCategory (cpf, id_category) VALUES ('22222222222', 24);
+INSERT INTO personCategory (cpf, id_category) VALUES ('33333333333', 1);
+INSERT INTO personCategory (cpf, id_category) VALUES ('33333333333', 18);
+INSERT INTO personCategory (cpf, id_category) VALUES ('33333333333', 16);
+INSERT INTO personCategory (cpf, id_category) VALUES ('44444444444', 13);
+INSERT INTO personCategory (cpf, id_category) VALUES ('44444444444', 23);
+INSERT INTO personCategory (cpf, id_category) VALUES ('44444444444', 11);
+INSERT INTO personCategory (cpf, id_category) VALUES ('55555555555', 11);
+INSERT INTO personCategory (cpf, id_category) VALUES ('55555555555', 15);
+INSERT INTO personCategory (cpf, id_category) VALUES ('55555555555', 1);
+INSERT INTO personCategory (cpf, id_category) VALUES ('66666666666', 15);
+INSERT INTO personCategory (cpf, id_category) VALUES ('66666666666', 6);
+INSERT INTO personCategory (cpf, id_category) VALUES ('66666666666', 21);
